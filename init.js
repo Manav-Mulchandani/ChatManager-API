1b21a0d93a0bfdd3e26c37ac46f9f2074f38493c
Manav-Mulchandani <mulchandanimanav01@gmail.com>
2026-06-18 00:17:09 +0530

    init.js data updated

--- init.js
+++ init.js
@@ -4,33 +4,27 @@ const Chat=require('./models/chat.js');
 main().then(res=>{console.log(res)}).catch(err => console.log(err));
 
 async function main() {
-  await mongoose.connect('mongodb://127.0.0.1:27017/whatsapp');
+  await mongoose.connect('mongodb://127.0.0.1:27017/chat_manager_api');
 
 }
 
 Chat.insertMany([
     {
+        from:'Neha',
+        to:'Karan',
+        msg:'Send me the project report and schedule a meeting today for further discussions',
+        created_at:new Date()
+    },
+    {
         from:'Manav',
-        to:'Mohit',
-        msg:'Exam kab hai teri?',
+        to:'Nikhil',
+        msg:'I have made the landing page of the project, do check it out once and let me know if any correction or update is required',
         created_at:new Date()
     },
     {
         from:'Nikhil',
-        to:'Ansh',
-        msg:'FastAPI ka code integrate karde',
-        created_at:new Date()
-    },
-    {
-        from:'Neel',
-        to:'Core Team',
-        msg:'Kab aa rahe ho bhai idhar?',
-        created_at:new Date()
-    },
-    {
-        from:'Manav',
-        to:'Client1',
-        msg:'Your website is deployed successfully',
+        to:'Manav',
+        msg:'I have seen the landing page, it looks perfect and there is no need for correction. Good job!',
         created_at:new Date()
     },
 ]);
